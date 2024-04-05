fac9dd0572357e7eceb518ac38250679346db6b7
helgamartens0 <helgamartens@icloud.com>
2024-04-05 10:36:14 -0300

    archivos tp6

---
+++ src/entidades/Producto.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entidades;
+
+/**
+ *
+ * @author helma
+ */
+public class Producto {
+
+    private int codigo;
+    private String descripcion;
+    private double precio;
+    private int stock;
+    private String rubro;
+
+    public Producto() {
+    }
+
+    public Producto(int codigo, String descripcion, double precio, int stock, String rubro) {
+        this.codigo = codigo;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.stock = stock;
+        this.rubro = rubro;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getCategoria() {
+        return rubro;
+    }
+
+    public void setCategoria(String rubro) {
+        this.rubro = rubro;
+    }
+
+}
